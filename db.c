ba6dcb68d493ca3a4e191eca3cf18cc87202d8fa
liujingyu <jingyu525@gmail.com>
2018-06-08 17:19:42 +0800

    feat: part6 done

--- db.c
+++ db.c
@@ -100,6 +100,13 @@ struct Table_t {
 typedef struct Table_t Table;
 
 
+struct Cursor_t {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table; // Inicates a position one past the last element
+};
+typedef struct Cursor_t Cursor;
+
 // 定义方法
 
 void pager_flush(Pager* pager, uint32_t page_num, uint32_t size) {
@@ -266,16 +273,6 @@ void* get_page(Pager* pager, uint32_t page_num) {
     return pager->pages[page_num];
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = get_page(table->pager, page_num);
-
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
-
-    return page + byte_offset;
-}
-
 Pager* pager_open(const char* filename) {
     int fd = open(filename,
             O_RDWR |   // Read/Write mode
@@ -329,6 +326,42 @@ void deserialize_row(void* source, Row* destination) {
 
 }
 
+Cursor* table_start(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+Cursor* table_end(Table* table) {
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+void* cursor_value(Cursor* cursor) {
+    uint32_t row_num = cursor->row_num;
+    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    void* page = get_page(cursor->table->pager, page_num);
+
+    uint32_t row_offset = row_num % ROWS_PER_PAGE;
+    uint32_t byte_offset = row_offset * ROW_SIZE;
+
+    return page + byte_offset;
+}
+
+void cursor_advance(Cursor* cursor) {
+    cursor->row_num += 1;
+    if (cursor->row_num >= cursor->table->num_rows) {
+        cursor->end_of_table = true;
+    }
+}
+
 ExexuteResult execute_insert(Statement* statement, Table* table) {
 
     if (table->num_rows >= TABLE_MAX_ROWS) {
@@ -337,19 +370,30 @@ ExexuteResult execute_insert(Statement* statement, Table* table) {
 
     Row* row_to_insert = &(statement->row_to_insert);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    Cursor* cursor = table_end(table);
+
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXIT_SUCCESS;
 }
 
 ExexuteResult execute_select(Statement* statement, Table* table) {
+    Cursor* cursor = table_start(table);
+
     Row row;
-    for (uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table, i), &row);
+
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
+
     }
-    
+
+    free(cursor);
+
     return EXIT_SUCCESS;
 }
 
